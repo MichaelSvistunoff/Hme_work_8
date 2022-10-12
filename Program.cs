3a256bb2afa37031c58f929b763610d93849570f
MichaelSv <michaelsvistunoff@gmail.com>
2022-10-12 06:52:43 +0600

    Задача 1: убрал задание из файла программы

--- Program.cs
+++ Program.cs
@@ -1,19 +1,4 @@
-﻿/*
-Задайте двумерный массив. 
-Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
-
-Например, задан массив:  
-1 4 7 2  
-5 9 2 3  
-8 4 2 4
-
-В итоге получается вот такой массив:  
-7 4 2 1  
-9 5 3 2  
-8 4 4 2
-*/
-
-Console.WriteLine("Enter number of rows of array ");
+﻿Console.WriteLine("Enter number of rows of array ");
 int rows = int.Parse(Console.ReadLine()!);
 
 Console.WriteLine("Enter number of columns of array ");
